c7a5330d6e9e4fe14b49fdaff9d83469b41e0e09
sowjanya <sowji.muppala@gmail.com>
2025-10-30 23:54:49 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -79,3 +79,43 @@ resource "terraform_data" "redis" {
     ]
   }
 }
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-rabbitmq"
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id,
+  ]
+  connection {
+    type        = "ssh"
+    user        = "ec2-user" # Or appropriate user for your AMI
+    password    = "DevOps321"
+    host        = aws_instance.rabbitmq.private_ip
+  }
+   # terraform copies this file to mongodb server
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh" # Path to the destination on the EC2 instance
+
+   }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
