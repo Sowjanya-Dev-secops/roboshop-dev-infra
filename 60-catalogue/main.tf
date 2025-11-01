a5f6c205c799e948eba15c5022ab2c3442f1b75c
sowjanya <sowji.muppala@gmail.com>
2025-11-01 23:33:00 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "catalogue" {
 # connect to instance using remote-excec provisioner through terraform_data
 resource "terraform_data" "catalogue" {
   triggers_replace = [
-    aws_instance.catalouge.id,
+    aws_instance.catalogue.id,
   ]
   connection {
     type        = "ssh"
