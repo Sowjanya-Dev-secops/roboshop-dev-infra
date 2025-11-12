b986fe9c55ada165d078c5234f2bdf57dd357e09
sowjanya <sowji.muppala@gmail.com>
2025-11-12 17:36:01 +0530

    practice

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -32,17 +32,6 @@ resource "aws_lb_listener" "backend_alb" {
   }
 }
 
-# resource "aws_route53_record" "backend_alb" {
-#   zone_id = var.zone_id
-#   name    = "*.backend_alb-${var.environment}.${var.domain_name}"
-#   type    = "A"
-
-#   alias {
-#     name                   = aws_lb.backend_alb.dns_name
-#     zone_id                = aws_lb.backend_alb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
   name    = "*.backend-alb-${var.environment}.${var.domain_name}"
