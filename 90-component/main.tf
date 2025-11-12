308318402d74ea3c4ab151d94ff88141503a645e
sowjanya <sowji.muppala@gmail.com>
2025-11-12 20:02:04 +0530

    practice

--- 90-component/main.tf
+++ 90-component/main.tf
@@ -1,5 +1,11 @@
-module "component" {
-  source = "../../terraform-roboshop-component"
-  component=var.component
-  rule_priority = var.rule_priority
+# module "component" {
+#   source = "../../terraform-roboshop-component"
+#   component=var.component
+#   rule_priority = var.rule_priority
+# }
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/Sowjanya-Dev-secops/terraform-roboshop-component.git?ref=min"
+    component=each.key
+    rule_priority = each.value.rule_priority
 }
