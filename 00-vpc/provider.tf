995524f181015c8ae2c938808649d8e2b5fad78d
sowjanya <sowji.muppala@gmail.com>
2025-11-16 00:09:57 +0530

    practice

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -9,7 +9,7 @@ terraform {
     bucket = "remote-state-sowji-dev"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
-    use_lockfile = true
+    use_lockfile = false
     encrypt = true
   }
 }
